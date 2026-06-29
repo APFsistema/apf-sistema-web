7e456eecbf8e5cde3b25a4715faaaf3f651234e9
APFsistema <penaflorsebastian297@gmail.com>
2026-06-29 15:53:17 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-// APF Marcador Online Torneo v61
-// Crear proyecto en Firebase, Realtime Database y pegar config acá.
-window.APF_FIREBASE_CONFIG = {
-  apiKey: "PEGAR_API_KEY",
-  authDomain: "PEGAR_AUTH_DOMAIN",
-  databaseURL: "PEGAR_DATABASE_URL",
-  projectId: "PEGAR_PROJECT_ID",
-  storageBucket: "PEGAR_STORAGE_BUCKET",
-  messagingSenderId: "PEGAR_MESSAGING_SENDER_ID",
-  appId: "PEGAR_APP_ID"
+window.APF_FIREBASE_CONFIG={
+apiKey:"PEGAR_API_KEY",
+authDomain:"PEGAR_AUTH_DOMAIN",
+databaseURL:"PEGAR_DATABASE_URL",
+projectId:"PEGAR_PROJECT_ID",
+storageBucket:"PEGAR_STORAGE_BUCKET",
+messagingSenderId:"PEGAR_MESSAGING_SENDER_ID",
+appId:"PEGAR_APP_ID"
 };
